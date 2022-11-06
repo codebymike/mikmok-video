dc6229394eb40aaa12460d75e1396ea21fa9f8a3
Mike Thomas <mikethomas84@gmail.com>
2022-11-06 22:17:59 +0000

    basic api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import axios from "axios"
+
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello World!!
     </h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await axios.get()
 }
