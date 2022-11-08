4f967c933918e2d2389af19ac9823db077627440
Mike Thomas <mikethomas84@gmail.com>
2022-11-08 23:19:07 +0000

    added priority to images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,6 +29,7 @@ const Navbar = () => {
                     src={Logo}
                     alt="Tiktik"
                     layout="responsive"
+                    priority
                 />
             </div>
         </Link>
@@ -52,6 +53,7 @@ const Navbar = () => {
                     alt='user'
                     width={40}
                     height={40}
+                    priority
                   />
                 </div>
               </Link>
