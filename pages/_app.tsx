0ea2472fac62203ed9cc2b5bd25068f3cf07c5a2
Mike Thomas <mikethomas84@gmail.com>
2022-11-08 21:24:48 +0000

    added Goog Oauth Prov

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
+import { useState, useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
-import { useState, useEffect } from 'react'
+import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
 
@@ -16,7 +17,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   if( isSSR ) return null
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -26,7 +27,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
 
