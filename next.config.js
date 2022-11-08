b640e2d12a9c41b3c27c6ffaead096f06bcf3d4a
Mike Thomas <mikethomas84@gmail.com>
2022-11-08 23:13:11 +0000

    new domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['randomuser.me']
+    domains: [
+      'randomuser.me',
+      'lh3.googleusercontent.com'
+    ]
   }
 }
 
