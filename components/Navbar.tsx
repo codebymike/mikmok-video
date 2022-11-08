23a73de7936a1d8ca62066bf49ee2907872b1088
Mike Thomas <mikethomas84@gmail.com>
2022-11-08 21:50:53 +0000

    basic login setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
@@ -9,6 +10,9 @@ import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+
+  const user = false
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -21,6 +25,18 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+
+        <div>Search</div>
+
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={ (res) => console.log(res) }
+            onError={ () => console.log('Error') }
+          />
+        )}
+
     </div>
   )
 }
