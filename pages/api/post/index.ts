a442bf016d3607c91a146a0e62509650157d6394
Mike Thomas <mikethomas84@gmail.com>
2022-11-15 19:28:18 +0000

    handle upload back

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -10,4 +10,10 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
 
         res.status(200).json(data)
     }
+    else if(req.method === "POST"){
+        const doc = req.body
+
+        client.create(doc)
+            .then(() => res.status(201).json('Video Created'))
+    }
 }
