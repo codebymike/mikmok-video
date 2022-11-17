f54e1ee253461a336e1d92c703f3a1001cac9c38
Mike Thomas <mikethomas84@gmail.com>
2022-11-17 17:23:10 +0000

    video detail init

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image';
+import Link from 'next/link';
+import axios from 'axios';
+
+import { GoVerified } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+
+import { BASE_URL } from '../../utils';
 
 const Detail = () => {
   return (
     <div>Detail</div>
   )
 }
+
+export const getServerSideProps = async ({
+    params: { id },
+  }: {
+    params: { id: string };
+  }) => {
+    const res = await axios.get(`${BASE_URL}/api/post/${id}`);
+  
+    return {
+      props: { postDetails: res.data },
+    };
+  };
 
 export default Detail
