054ff9093b415c313b40b3f3b7a79aad04f68095
Mike Thomas <mikethomas84@gmail.com>
2022-11-25 16:15:17 +0000

    profile api

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { GoVerified } from 'react-icons/go';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { GoVerified } from 'react-icons/go'
+import axios from 'axios'
 
-import VideoCard from '../../components/VideoCard';
-import NoResult from '../../components/NoResult';
-import { IUser, Video } from '../../types';
-import { BASE_URL } from '../../utils';
+import VideoCard from '../../components/VideoCard'
+import NoResult from '../../components/NoResult'
+import { IUser, Video } from '../../types'
+import { BASE_URL } from '../../utils'
 
 const Profile = () => {
     return (
@@ -17,13 +17,13 @@ const Profile = () => {
 export const getServerSideProps = async ({
     params: { userId },
   }: {
-    params: { userId: string };
+    params: { userId: string }
   }) => {
-    const res = await axios.get(`${BASE_URL}/api/profile/${userId}`);
+    const res = await axios.get(`${BASE_URL}/api/profile/${userId}`)
   
     return {
       props: { data: res.data },
-    };
-  };
+    }
+  }
 
 export default Profile
