e8bd653c87d01eb294301735642d0c7176f99702
Mike Thomas <mikethomas84@gmail.com>
2022-11-25 16:12:06 +0000

    initial imports

---
+++ pages/profile/[id].tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { GoVerified } from 'react-icons/go';
+import axios from 'axios';
+
+import VideoCard from '../../components/VideoCard';
+import NoResult from '../../components/NoResult';
+import { IUser, Video } from '../../types';
+import { BASE_URL } from '../../utils';
+
+const Profile = () => {
+    return (
+        <div>Profile</div>
+    )
+}
+
+export const getServerSideProps = async ({
+    params: { userId },
+  }: {
+    params: { userId: string };
+  }) => {
+    const res = await axios.get(`${BASE_URL}/api/profile/${userId}`);
+  
+    return {
+      props: { data: res.data },
+    };
+  };
+
+export default Profile
