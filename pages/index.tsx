d43f9b5beae6136c8d4d5db6d0a0a80f4ee986e6
Mike Thomas <mikethomas84@gmail.com>
2022-11-29 20:26:05 +0000

    topic search

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import axios from "axios"
 import { Video } from "../types"
 import VideoCard from "../components/VideoCard"
 import NoResult from "../components/NoResult"
+import { BASE_URL } from '../utils'
 
 interface IProps {
   videos: Video[]
@@ -23,12 +24,20 @@ export default function Home({ videos } : IProps) {
   )
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`http://localhost:3000/api/post`)
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string }
+}) => {
+  let response = null
 
-  return {
-    props: {
-      videos: data
-    }
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`)
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`)
   }
-}
+  
+  return {
+    props: { videos: response.data },
+  }
+};
